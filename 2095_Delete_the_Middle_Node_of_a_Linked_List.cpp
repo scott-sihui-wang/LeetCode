500e5c0faa5fcbc17c6055c6ddbf2cfb353fae23
unknown <scott_sihui_wang@yahoo.com>
2024-04-22 14:08:42 -0700

    LC2095M overall solution correct, detail: == vs =

---
+++ 2095_Delete_the_Middle_Node_of_a_Linked_List.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        ListNode *slow=head,*slow_prev=head,*fast=head;
+        if(slow->next==nullptr){
+            return nullptr;
+        }
+        while(fast!=nullptr && fast->next!=nullptr){
+            slow_prev=slow;
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        slow_prev->next=slow->next;
+        return head;
+    }
+};
