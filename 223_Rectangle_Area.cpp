847d905d5d1ae20993aed61da35e8c5b3727d09a
unknown <scott_sihui_wang@yahoo.com>
2024-05-17 13:23:32 -0700

    LC223M runtime improved

--- 223_Rectangle_Area.cpp
+++ 223_Rectangle_Area.cpp
@@ -5,17 +5,8 @@ public:
         ans+=(ax2-ax1)*(ay2-ay1);
         ans+=(bx2-bx1)*(by2-by1);
         int intersection=0;
-        if(bx1<ax1 && by1<ay1 && bx2>ax1 && by2>ay1){
-            intersection=(min(bx2,ax2)-ax1)*(min(ay2,by2)-ay1);
-        }
-        else if(bx1<ax1 && by1<ay2 && by1>=ay1 && bx2>ax1){
-            intersection=(min(ay2,by2)-by1)*(min(ax2,bx2)-ax1);
-        }
-        else if(bx1<ax2 && bx1>=ax1 && by1<ay1 && by2>ay1){
-            intersection=(min(ay2,by2)-ay1)*(min(ax2,bx2)-bx1);
-        }
-        else if(bx1>=ax1 && by1>=ay1 && bx1<ax2 && by1<ay2){
-            intersection=(min(ay2,by2)-by1)*(min(ax2,bx2)-bx1);
+        if(bx1<ax2 && by1<ay2 && bx2>ax1 && by2>ay1){
+            intersection=(min(bx2,ax2)-max(ax1,bx1))*(min(ay2,by2)-max(ay1,by1));
         }
         ans-=intersection;
         return ans;
