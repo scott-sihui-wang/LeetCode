9a89e2c97554d08b1aaca682bdd95f84485061de
unknown <scott_sihui_wang@yahoo.com>
2024-05-17 15:31:29 -0700

    LC284M

---
+++ 284_Peeking_Iterator.cpp
@@ -0,0 +1,56 @@
+/*
+ * Below is the interface for Iterator, which is already defined for you.
+ * **DO NOT** modify the interface for Iterator.
+ *
+ *  class Iterator {
+ *		struct Data;
+ * 		Data* data;
+ *  public:
+ *		Iterator(const vector<int>& nums);
+ * 		Iterator(const Iterator& iter);
+ *
+ * 		// Returns the next element in the iteration.
+ *		int next();
+ *
+ *		// Returns true if the iteration has more elements.
+ *		bool hasNext() const;
+ *	};
+ */
+
+class PeekingIterator : public Iterator {
+public:
+    bool has_next;
+    int next_val;
+	PeekingIterator(const vector<int>& nums) : Iterator(nums) {
+	    // Initialize any member here.
+	    // **DO NOT** save a copy of nums and manipulate it directly.
+	    // You should only use the Iterator interface methods.
+        if(Iterator::hasNext()){
+            has_next=true;
+            next_val=Iterator::next();
+        }
+        else{
+            has_next=false;
+        }
+	}
+	
+    // Returns the next element in the iteration without advancing the iterator.
+	int peek() {
+        return next_val;
+	}
+	
+	// hasNext() and next() should behave the same as in the Iterator interface.
+	// Override them if needed.
+	int next() {
+        int val=next_val;
+        has_next=Iterator::hasNext();
+        if(has_next){
+            next_val=Iterator::next();
+        }
+        return val;
+	}
+	
+	bool hasNext() const {
+	    return has_next;
+	}
+};
