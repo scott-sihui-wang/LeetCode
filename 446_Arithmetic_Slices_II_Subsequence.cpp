266e1c1d089b863a5c4fcf0c5eabf3843534ae66
unknown <scott_sihui_wang@yahoo.com>
2024-05-25 11:48:55 -0700

    LC446H Improved runtime

--- 446_Arithmetic_Slices_II_Subsequence.cpp
+++ 446_Arithmetic_Slices_II_Subsequence.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     int numberOfArithmeticSlices(vector<int>& nums) {
-        map<int,map<long long,int>>dp;
-        for(int i=1;i<nums.size();i++){
-            for(int j=0;j<i;j++){
-                dp[i][(long long)(nums[i])-(long long)(nums[j])]+=1+dp[j][(long long)(nums[i])-(long long)(nums[j])];
-            }
-        }
-        int ans=0;
-        for(auto iter1=dp.begin();iter1!=dp.end();iter1++){
-            for(auto iter2=iter1->second.begin();iter2!=iter1->second.end();iter2++){
-                ans+=iter2->second;
-            }
-        }
         int n=nums.size();
-        ans-=n*(n-1)/2;
+        vector<map<long,int>>dp(n);
+        int ans=0;
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                long diff=(long)nums[i]-(long)nums[j];
+                if(dp[j].find(diff)!=dp[j].end()){
+                    dp[i][diff]+=1+dp[j][diff];
+                    ans+=dp[j][diff];
+                }
+                else{
+                    dp[i][diff]+=1;
+                }
+            }
+        }
         return ans;
     }
 };
